141ca160f6e7393b0a19c9e7713dce3e823f1aae
samuelbohl <samuel.bohl@gmail.com>
2022-04-07 12:52:33 +0200

    fixed recomendation time + loading animation

--- client/src/Connection.js
+++ client/src/Connection.js
@@ -4,17 +4,43 @@ import axios from 'axios';
 import {Slider,  Row, Col, Button} from 'antd';
 import moment from 'moment';
 import { Line } from '@ant-design/plots';
+import ReactLoading from 'react-loading';
 
 
 function Connection(props) {
 
 
     const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [recomendation, setRecomendation] = useState('Last Minute');
 
     useEffect(() => {
         axios.get('http://localhost:8000/prob/' + props.trainNum + '/' + props.fromId + '/' + props.toId + '/' + moment(props.date).format("YYYY-MM-DD") + '/1', {mode: 'no-cors'})
         .then(res => {
             setData(() => res.data.map((el) => ({...el, delta: el.delta/60000})))
+            setLoading(() => false)
+
+            res.data.some((dataPoint, index) => {
+                console.log(dataPoint)
+                if(dataPoint.prob > 0.65) {
+                    let departureTimeAndDateMoment = moment(moment(props.date).format("YYYY-MM-DD") + ' ' + props.depTime)
+                    departureTimeAndDateMoment.subtract(moment.duration(-1 * dataPoint.delta))
+
+                    if(index != 0) {
+                        setRecomendation(() => departureTimeAndDateMoment.format("HH:mm DD.MM.YYYY"))
+                    } else {
+                        setRecomendation(() => 'ASAP')
+                    }
+                    console.log(departureTimeAndDateMoment)
+                    console.log(index)
+                    return true;
+                }
+                return false;
+            })
+
+            if(res.data.length <= 1) {
+                setRecomendation(() => 'No Data')
+            }
             console.log(res.data)
         })
     }, [])
@@ -46,8 +72,17 @@ function Connection(props) {
                 <span style={{fontSize: 18, fontWeight: 700}}>{props.depTime + ' '}<Slider disabled range defaultValue={[0, 100]}/>{' ' + props.arrTime}</span>
             </Row>
         </Col>
-        <Col span={4}><Line width={300} height={100} {... config} /></Col>
-        <Col span={4} style={{fontSize: 16, textAlign: 'center', paddingTop: 28}}>Recomended latest reservation time: {props.depTime}</Col>
+        <Col span={4} style={{width: 300}}>
+            {loading &&
+                <ReactLoading type={'spin'} color={'#eb0000'} height={80} width={80} />
+            }
+            {!loading &&
+                <Line width={300} height={100} {... config} />
+            }
+        </Col>
+        <Col span={4} style={{fontSize: 16, textAlign: 'center', paddingTop: 28}}>
+            Recomended latest reservation time: {recomendation}
+        </Col>
         <Col span={2} style={{paddingTop: 32}}>
             <Button type="primary" href={reservationUrl} target="_blank" style={{backgroundColor: '#eb0000', border: 'none', borderRadius: '.13333em', height: '44px', padding: '1em 2.66667em 1em'}}>Reserve Now</Button>
         </Col>
